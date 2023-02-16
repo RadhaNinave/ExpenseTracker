66de714c1db1e2645cd871b68d27e239d226295b
RadhaNinave <radhikarao245@gmail.com>
2023-02-16 15:03:01 +0530

    verifyEmail

--- src/Component/UI/Welcome.js
+++ src/Component/UI/Welcome.js
@@ -1,9 +1,39 @@
 import { Fragment } from "react"
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container,Button } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import './Welcome.css';
 
 const Welcome = () =>{
+      const verifyEmail = async(e) =>{
+        e.preventDefault();
+        try{
+        const res = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAXiOYJr-9lsF_yoZxTgE3AByKyl3bGlqw",{
+          method:"POST",
+          body:JSON.stringify({
+            idToken:JSON.parse(localStorage.getItem("idToken")).idToken,
+            requestType:"VERIFY_EMAIL"
+          }),
+          headers :{
+            'Content-Type': 'application/json'
+          }
+        })
+        const data = await res.json();
+        console.log(data)
+        if(res.ok){
+          alert("Email verify successfully")
+        }
+       
+        }
+        catch(e){
+        console.log("try again");
+        }
+      }
+     
+    
+
+
+      
+
     return (
        <Fragment>
      <Navbar>
@@ -18,6 +48,9 @@ const Welcome = () =>{
       </Container>
     </Navbar>
         <hr />
+        <h1>
+          <Button variant="success" onClick={verifyEmail}>Verify Email</Button>
+        </h1>
        </Fragment>
     )
 }
