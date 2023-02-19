66aef3bbc0ca416f4d27f90736584d8bb9f1f095
RadhaNinave <radhikarao245@gmail.com>
2023-02-19 17:08:13 +0530

    adding edit delete functionality

--- src/Component/Auth/Expense.js
+++ src/Component/Auth/Expense.js
@@ -2,90 +2,147 @@ import { useEffect, useRef, useState } from "react";
 import "./Expense.css";
 import axios from "axios";
 import { json } from "react-router-dom";
-
+import AuthContext from '../store/AuthContext';
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Table } from "react-bootstrap";
 const Expense = () => {
-  const amountInputRef = useRef();
-  const descInputRef = useRef();
-  const categoryInputRef = useRef();
-   const [expense, setExpenses] = useState([]);
-    
-   const addExpense = async(e) =>{
-    e.preventDefault();
-    const obj = {
-      amount :amountInputRef.current.value,
-      desc:descInputRef.current.value,
-      category:categoryInputRef.current.value
-    }
-    try{
-      const res=await fetch('https://expensetracker-1498c-default-rtdb.firebaseio.com/expenses.json',{
-      method:'POST',
-      Headers:{
-        'content-type':'application/json'
-      },
-      body:JSON.stringify(obj)
-     })
-     const data= await res.json();
-     if(res.ok){
-        alert('expense added Successfully');
-        await fetchExpenses();
-     }
-     else{
-      throw data.error;
-     }
-    }
-    catch(error){
-      console.log(error)
-    }
 
-  }
-  const fetchExpenses = async() =>{
-    try{
+   const AuthContextToken = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [data, setUserData] = useState([]);
 
-      const res=await fetch('https://expensetracker-1498c-default-rtdb.firebaseio.com/expenses.json',{
-        method:'GET',
-        headers:{
-          'content-type':'aplication/json'
-        },
-       
-      })
-      const data=await res.json();
-      console.log(data);
-      if(res.ok){
-        const newdata=[];
-        for(let key in data){
-          newdata.push({id:key,...data[key]})
-        }
-        setExpenses(newdata);
-       
-      }
-      else{
-        throw data.error
-      }
-    }
-    catch(e){
-
-      console.log(e.message);
-    }
-
-
-
-  }
+   
+    const [amount, setAmount] = useState('');
+    const [description, setDescription] = useState('');
+    const [catagory, setCatagory] = useState('');
+    const [id,setId]=useState(null);
+    const [isEditing,setisEditting]=useState(false);
  
-  useEffect(()=>{
-    fetchExpenses()
-  },[])
+    function onAmountHandler(event) {
+      setAmount(event.target.value);
+  }
+  function onDescriptionHandler(event) {
+      setDescription(event.target.value);
+  }
 
+  const onCatagoryHandler = (e) => {
+      setCatagory(e.target.value);
+  };
+
+
+    const submitHandler = (e) =>{
+      e.preventDefault();
+      //const enteredAmount = Amount.current.value
+      //const enteredDesc = Desc.current.value
+      //const enteredCategory = category.current.value
+      
+      const auth = AuthContextToken.email
+      const replaceEmail = auth.replace('.', '')
+
+      if(!isEditing){
+
+      
+        fetch(`https://expensetracker-1498c-default-rtdb.firebaseio.com/${replaceEmail}.json`, {
+            method: 'POST',
+            body: JSON.stringify({  amount: amount, description: description, catagory: catagory }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        alert('Your Item added Successfully')
+    }else{
+        fetch(`https://expensetracker-1498c-default-rtdb.firebaseio.com/${authreplaced}/${id}.json`, {
+            method: "PUT",
+            body: JSON.stringify({
+
+               
+                amount: amount,
+                catagory: catagory,
+                description: description,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => {
+            alert("please Update Your Expence")
+        });
+        setisEditting(false)
+    }
+
+
+
+    }
+    const auth = AuthContextToken.email;
+    const authreplaced = auth.replace('.', '');
+    // console.log(authreplaced)
+
+
+
+
+    let UserData = [];
+    useEffect(() => {
+
+
+        fetch(`https://expensetracker-1498c-default-rtdb.firebaseio.com/${authreplaced}.json`, {
+            method: 'GET',
+        }).then((res) => {
+            return res.json().then((res) => {
+
+                // console.log(res)
+
+                for (const key in res) {
+
+                    UserData.push({
+                        id: key,
+                       
+                        amount: res[key].amount,
+                        description: res[key].description,
+                        catagory: res[key].catagory,
+                    })
+                }
+                setUserData(UserData);
+                toDeleteDataHandler()
+
+            })
+        })
+    }, [])
+
+    const toDeleteDataHandler = (id) => {
+      // console.log(id)
+      fetch(`https://expensetracker-1498c-default-rtdb.firebaseio.com/${authreplaced}/${id}.json`, {
+          method: 'DELETE',
+      }).then((res) => {
+          if (res.ok) {
+              alert("Your Item Delete Please Refresh The Page")
+
+          }
+
+      })
+  }
+  const editHandlerHandler = (id,amount1,catagory1,description1) => {
+    setId(id)
+  
+    setAmount(amount1);
+    setCatagory(catagory1)
+    setDescription(description1);
+    setisEditting(true)
+
+    
+}
+    
   return (
     <div>
-      <form className="form-expenses" onSubmit={addExpense}>
+      <form className="form-expenses" onSubmit={submitHandler}>
         <label htmlFor="amount">Amount</label>
-        <input ref={amountInputRef} type="number" />
+        <input onChange={onAmountHandler} type="number" value={amount}/>
 
         <label htmlFor="desc">Description</label>
-        <textarea type="text" rows="3" ref={descInputRef}></textarea>
+        <textarea type="text" rows="3" onChange={onDescriptionHandler} value={description}></textarea>
         <label htmlFor="category">Choose a car:</label>
         <select
-          ref={categoryInputRef}
+         onChange={onCatagoryHandler}
+          value={catagory}
           id="category"
           //   style={{
           //     display: "block",
@@ -104,12 +161,55 @@ const Expense = () => {
       </form>
       <h1>Items</h1>
      
-      <ul>
-        {expense && expense.length > 0 && expense.map((userObj, index) => (
-            <li key={userObj.id}>{userObj.amount}----
-            {userObj.desc} ---{userObj.category} <br></br></li>
-          ))}
-      </ul>
+      <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                   
+                    <th>Amount</th>
+                    <th>Catagory</th>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th >Delete</th>
+                </tr>
+            </thead>
+
+            {data.map((currvalue, index) => {
+                return <tbody>
+                    <tr>
+                        <td>{index + 1}</td>
+                        
+                        <td>{currvalue.amount}</td>
+                        <td>{currvalue.catagory}</td>
+                        <td>{currvalue.description}</td>
+                        <td>
+                            <button
+                                type="button"
+                                className="btn btn-warning"
+                                onClick={editHandlerHandler.bind(null, 
+                                    currvalue.id,
+                                  
+                                    currvalue.amount,
+                                    currvalue.catagory,
+                                    currvalue.description,
+                                    )} >
+                                Edit
+
+                            </button>
+                        </td>
+                        <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={toDeleteDataHandler.bind(null, 
+                                currvalue.id)}
+                        >
+                            Delete
+                        </button>
+                    </tr>
+                </tbody>
+            })}
+
+        </Table>
      
     </div>
   );
--- src/Component/store/AuthContext.js
+++ src/Component/store/AuthContext.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const AuthContext = React.createContext({
+    email :"",
     token : "",
     isLoggedIn : false,
     login : (token)=>{},
@@ -9,23 +10,28 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider=(props)=>{
     const initialToken = localStorage.getItem('token')
+    const initialEmail = localStorage.getItem('email');
     console.log(initialToken);
     const[token , setToken] = useState(initialToken)
+    const[email, setEmail] = useState(initialEmail)
 
     const userLoggedIn = !!token;
     const loginHandler =(token)=>{
     localStorage.setItem('token' , token)
-   
+   localStorage.setItem('email',email)
      setToken(token)
+     setEmail(email)
 
     }
     const logoutHandler = ()=>{
         
         setToken(null);
         localStorage.removeItem('idToken')
+
     }
 
     const contextValue = {
+        email:email,
         token : token,
         isLoggedIn : userLoggedIn,
         login : loginHandler,
